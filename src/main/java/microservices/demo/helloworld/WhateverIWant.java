251c8e642c24b2a600b7a6f4e1c520b4ecc6ee6b
cuongbk <cuongbk@fsoft.com.vn>
2017-12-17 13:09:18 +1100

    Add logging with logback.

--- src/main/java/microservices/demo/helloworld/WhateverIWant.java
+++ src/main/java/microservices/demo/helloworld/WhateverIWant.java
@@ -12,9 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import microservices.demo.helloworld.domain.Player;
 import microservices.demo.helloworld.domain.Team;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class WhateverIWant {
 	private Team team;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(WhateverIWant.class);
 	
 	@PostConstruct
 	public void init() {
@@ -27,6 +32,11 @@ public class WhateverIWant {
 	
 	@RequestMapping("/hi")
 	public @ResponseBody Team hiThere() {
+		LOGGER.trace("doStuff needed more information");
+	    LOGGER.debug("doStuff needed to debug");
+	    LOGGER.info("doStuff took input ");
+	    LOGGER.warn("doStuff needed to warn ");
+	    LOGGER.error("doStuff encountered an error with value - {}", team);
 		return team;
 	}
 }
